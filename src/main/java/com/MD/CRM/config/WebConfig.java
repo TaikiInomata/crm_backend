0d4407d98c41fc912fe719724525dbdc0e8e504f
Novi <thanhdungng04@gmail.com>
2025-11-01 11:34:58 +0700

    Fix: Cors

---
+++ src/main/java/com/MD/CRM/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.MD.CRM.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Cho phép tất cả API
+                        .allowedOrigins("*") // Cho phép tất cả origin (Swagger, frontend,...)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
